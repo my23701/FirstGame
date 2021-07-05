99240a55aa26ee44fb386e6e3d1b842a7e5468a8
my23701 <65496483+my23701@users.noreply.github.com>
2021-07-05 10:58:28 +0530

    Add files via upload

---
+++ Assets/Scripts/PlayerColiision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerColiision : MonoBehaviour
+{
+    public Playermovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag=="Obstacle")
+        {
+            movement.enabled=false;
+        }
+    }
+}
---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    public float forwardforce=2000f;
+    public float sideforce=500f;
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        rb.AddForce(0,0,forwardforce*Time.deltaTime);
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sideforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+    }
+}
